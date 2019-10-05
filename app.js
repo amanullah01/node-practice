a660cb506f27b1f8e25de5dee2e3bce30364fc53
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-05 18:39:17 +0600

    Express js added

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
 const http = require("http");
 
-const routes = require("./routes");
-console.log(routes.someText);
-
-// const server = http.createServer(routes);
-const server = http.createServer(routes.handler);
-
-/*
-const server = http.createServer((req, res) => {
-  //console.log("hello world");
-  //Request
-  //console.log(req.url, req.method, req.headers);
-  //this is for process exit or stop program 
-  //process.exit();
-  //Response
-});
-*/
+const server = http.createServer();
 
 server.listen(3000);
