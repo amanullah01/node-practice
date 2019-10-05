efbc8479759ffc6fbfa4ff426196a6735ab24235
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-05 13:09:04 +0600

    sending response

--- 03.The_basic/app.js
+++ 03.The_basic/app.js
@@ -4,6 +4,14 @@ const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
   /**this is for process exit or stop program */
   //   process.exit();
+
+  //Response
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first node page</title></head>");
+  res.write("<body><h1>Hello from node js server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
