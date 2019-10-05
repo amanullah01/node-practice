2abf0e821da9f28b3eded6174a2dd15c6acb5129
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-05 19:19:31 +0600

    body-parser, incoming request

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const http = require("http");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //middleware function
-app.use("/", (req, res, next) => {
-  console.log("This is always run!");
-  next();
-});
 
 app.use("/add-product", (req, res, next) => {
-  console.log("Add product middleware!");
-  res.send("<h1>Add product page</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
   //response
   res.send("<h1>Hello from express</h1>");
 });
