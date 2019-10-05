10c5a01e26142a1d2dbd52b79d86f40f3cbd0281
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-05 18:53:04 +0600

    add middleware in express js

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const http = require("http");
 
-const server = http.createServer();
+const express = require("express");
+
+const app = express();
+
+//middleware function
+app.use((req, res, next) => {
+  console.log("In middleware!");
+  //allows the request to continue to the next middleware in line
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
