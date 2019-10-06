160faa73d92a9281b8487da900c211acf8ee4497
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-06 20:10:42 +0600

    add a comment in app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"))); // for static file from public folder
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
