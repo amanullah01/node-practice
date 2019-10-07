aaff55f7a930ec39c0567e40f60f5e27436ac007
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-07 13:50:29 +0600

    handlebars template engine implement start and 404 page in hbs format

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars"); // this is for handlebars template
 
 const app = express();
 
+app.engine("hbs", exphbs({ defaultLayout: null })); // this is only for handlebars
+// app.engine(".hbs", expressHbs({ extname: ".hbs" }));
 // view engine added (PUG)
-app.set("view engine", "pug");
+//app.set("view engine", "pug");
+
+// view engine added (handlebars)
+app.set("view engine", "hbs");
 app.set("views", "views"); // from views folder find my dynamic templates
 
 // import from routes folder
@@ -21,7 +27,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   //res.status(404).send("<h1>Page not found</h1>");
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  //res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
   res.status(404).render("404", { pageTitle: "404" });
 });
 
