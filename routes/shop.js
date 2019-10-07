f03580a6b2bac756633ca4fa85d58eafbb1e8678
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-07 12:51:57 +0600

    Outputting dynamic content in pug template

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,9 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   console.log(adminData.products);
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
-  res.render("shop"); //load pug file
+
+  const products = adminData.products;
+  res.render("shop", { prods: products, title: "Shop" }); //load pug file
 });
 
 module.exports = router;
