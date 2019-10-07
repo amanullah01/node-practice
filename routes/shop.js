6821297f1115f1ed0f2c2a867325bf79c1d47cf4
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-07 13:17:50 +0600

    Finish pug

--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,7 @@ router.get("/", (req, res, next) => {
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
 
   const products = adminData.products;
-  res.render("shop", { prods: products, pageTitle: "Shop" }); //load pug file
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" }); //load pug file
 });
 
 module.exports = router;
