579252d97faa299a265fd9331f0b16770554cce5
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-08 21:57:28 +0600

    data save in a json file and retrive from it and show it in homepage

--- models/product.js
+++ models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const dir = require("../util/path");
 
 module.exports = class Product {
   constructor(t) {
@@ -6,10 +9,27 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    //products.push(this);
+    const filePath = path.join(dir, "data", "products.json");
+    fs.readFile(filePath, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(filePath, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const filePath = path.join(dir, "data", "products.json");
+    fs.readFile(filePath, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
   }
 };
