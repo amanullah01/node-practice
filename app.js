67bbdc3e86bb5cb3fbc851d71797d414dfc72c4a
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-08 18:56:24 +0600

    ejs implement

--- app.js
+++ app.js
@@ -2,24 +2,23 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars"); // this is for handlebars template
+//const exphbs = require("express-handlebars"); // this is for handlebars template
 
-const app = express();
+const app = express(); // this is only for handlebars. defaultLayout: null
 
-app.engine(
+/*app.engine(
   "hbs",
   exphbs({
     layoutsDir: "views/layouts/",
     defaultLayout: "main-layout",
     extname: ".hbs"
   })
-); // this is only for handlebars. defaultLayout: null
-// app.engine(".hbs", expressHbs({ extname: ".hbs" }));
+);*/ // app.engine(".hbs", expressHbs({ extname: ".hbs" }));
 // view engine added (PUG)
 //app.set("view engine", "pug");
 
 // view engine added (handlebars)
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views"); // from views folder find my dynamic templates
 
 // import from routes folder
