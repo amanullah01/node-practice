903a1993305e0ea69b3c667572fee58244813df5
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-16 12:53:07 +0600

    sync js definitions to the database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
 
 const app = express(); // this is only for handlebars. defaultLayout: null
 app.set("view engine", "ejs");
@@ -20,4 +21,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+//db
+sequelize
+  .sync()
+  .then(result => {
+    //console.log(result);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
