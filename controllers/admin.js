3bc0a784751e0f9aabbdede8534a07a6b1712cbe
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-16 15:29:56 +0600

    Fetcing related Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -36,8 +36,11 @@ exports.getEditProduct = (req, res, next) => {
   }
 
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+  //Product.findByPk(prodId);
+  req.user
+    .getProducts({ where: { id: prodId } })
+    .then(products => {
+      const product = products[0];
       if (!product) {
         return redirect("/");
       }
@@ -78,7 +81,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  // Product.findAll()
+  req.user
+    .getProducts()
     .then(products => {
       res.render("admin/products", {
         prods: products,
