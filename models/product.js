cf98337fd77804c3b60fc7da1c8304b119208c92
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-16 12:13:58 +0600

    Insert into database

--- models/product.js
+++ models/product.js
@@ -28,7 +28,10 @@ module.exports = class Product {
   }
 
   save() {
-    //create product id
+    return db.execute(
+      "INSERT INTO products (title,price,description,imageUrl) VALUES (?,?,?,?)",
+      [this.title, this.price, this.description, this.imageUrl]
+    );
   }
 
   static deleteById(id) {}
