fc665be81f80300b3dd69847796c6f3e32dc7ef6
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-21 12:41:39 +0600

    connect mongodb with our application

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const errorController = require("./controllers/error");
-const sequelize = require("./util/database");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+const mongoConnect = require("./util/database");
 
 const app = express(); // this is only for handlebars. defaultLayout: null
 app.set("view engine", "ejs");
@@ -17,58 +12,26 @@ app.set("views", "views"); // from views folder find my dynamic templates
 
 //middleware
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+  // User.findByPk(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
 });
 // import from routes folder
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public"))); // for static file from public folder
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-//relation/association
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-//db
-//force: true delete all data
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then(result => {
-    return User.findByPk(1);
-    //console.log(result);
-  })
-  .then(user => {
-    if (!user) {
-      return User.create({ name: "Aman", email: "aman@softograph.com" });
-    }
-    return user;
-  })
-  .then(user => {
-    return user.createCart();
-  })
-  .then(cart => {
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+mongoConnect(client => {
+  console.log(client);
+  app.listen(3000);
+});
--- util/database.js
+++ util/database.js
@@ -1,20 +1,16 @@
-const Sequelize = require("sequelize").Sequelize;
+//mongo  db
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "", {
-  dialect: "mysql",
-  host: "localhost"
-});
+const mongoConnect = callback => {
+  MongoClient.connect(
+    "mongodb+srv://aman:aman2626@cluster-node-practice-nqjyv.mongodb.net/test?retryWrites=true&w=majority"
+  )
+    .then(result => {
+      console.log("connected");
+      callback(result);
+    })
+    .catch(err => console.log(err));
+};
 
-module.exports = sequelize;
-
-/*const mysql = require("mysql2");
-
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: ""
-});
-
-module.exports = pool.promise();
-*/
+module.exports = mongoConnect;
