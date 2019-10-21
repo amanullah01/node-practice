b8889237209a76842cd4303bee56a326b9919a5a
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-21 18:53:49 +0600

    show total amount in orders page

--- models/product.js
+++ models/product.js
@@ -4,7 +4,7 @@ const getDb = require("../util/database").getDb;
 class Product {
   constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
-    this.price = price;
+    this.price = parseFloat(price);
     this.description = description;
     this.imageUrl = imageUrl;
     this._id = id ? new mongodb.ObjectID(id) : null;
