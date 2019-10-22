834b818409e23652ac9995ab4c710f887957acef
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-22 13:36:46 +0600

    Add user model

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express(); // this is only for handlebars. defaultLayout: null
 app.set("view engine", "ejs");
 app.set("views", "views"); // from views folder find my dynamic templates
 
 //middleware
-// app.use((req, res, next) => {
-//   User.findById("5dad7b210c144e1a5492922d")
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("5daeb00226e1173728b39fe0")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 // import from routes folder
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -38,6 +38,21 @@ mongoose
   )
   .then(result => {
     console.log("mongoose connected!!!!");
+    User.findOne()
+      .then(user => {
+        if (!user) {
+          const user = new User({
+            name: "Amanullah Aman",
+            email: "aman@softograph.com",
+            cart: {
+              items: []
+            }
+          });
+          user.save();
+        }
+      })
+      .catch(err => console.log(err));
+
     app.listen(3000);
   })
   .catch(err => console.log(err));
