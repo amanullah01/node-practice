4924b4512b74727514d627bb76e5e72319da1494
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-22 16:01:14 +0600

    get orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -111,9 +111,9 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ "user.userId": req.user._id })
     .then(orders => {
+      console.log(orders);
       res.render("shop/orders", {
         pageTitle: "Your orders",
         path: "/order",
