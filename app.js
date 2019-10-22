90a902d404dbb79babfae062204b27e1a1096ca7
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-22 13:05:22 +0600

    Saving product through mongoose

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const User = require("./models/user");
+// const User = require("./models/user");
 
 const app = express(); // this is only for handlebars. defaultLayout: null
 app.set("view engine", "ejs");
 app.set("views", "views"); // from views folder find my dynamic templates
 
 //middleware
-app.use((req, res, next) => {
-  User.findById("5dad7b210c144e1a5492922d")
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("5dad7b210c144e1a5492922d")
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 // import from routes folder
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
