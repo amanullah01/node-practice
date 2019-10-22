3bdf0d1bee4e33372bcbb7f543a066323026ed3e
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-22 17:34:14 +0600

    Setting up a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,18 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req
+    .get("Cookie")
+    .split(";")[1]
+    .trim()
+    .split("=")[1];
   res.render("auth/login", {
     pageTitle: "Login Page",
     path: "/login",
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: isLoggedIn
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  //   req.isLoggedIn = true;
+  res.setHeader("Set-Cookie", "loggedIn=true");
   res.redirect("/");
 };
