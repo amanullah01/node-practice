07e94ad6755d7bc19cffd47d7b1803d0d4e0df98
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-22 15:47:35 +0600

    Order model created and stroing order data

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -48,7 +49,6 @@ exports.getCart = (req, res, next) => {
     .populate("cart.items.productId")
     .execPopulate()
     .then(user => {
-      // console.log(user.cart.items);
       const products = user.cart.items;
       res.render("shop/cart", {
         pageTitle: "Your Cart",
@@ -84,7 +84,24 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
   req.user
-    .addOrder()
+    .populate("cart.items.productId")
+    .execPopulate()
+    .then(user => {
+      console.log(user.cart.items);
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, productData: { ...i.productId._doc } };
+      });
+
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+
+      return order.save();
+    })
     .then(result => {
       res.redirect("/orders");
     })
