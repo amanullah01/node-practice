7ff5f39662cfe37134daad9fe0f027f509c0d354
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-22 13:23:50 +0600

    Delete product using findByIdAndRemove

--- controllers/admin.js
+++ controllers/admin.js
@@ -92,7 +92,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log("product has been deleted");
       res.redirect("/admin/products");
