ee36decb34ca3c9e305068bf18bf6a95a3927f62
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-23 13:02:39 +0600

    CSRF token uses

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDbStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -17,6 +18,8 @@ const store = new MongoDbStore({
   uri: MONGODB_URI,
   collection: "sessions"
 });
+
+const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
 app.set("views", "views"); // from views folder find my dynamic templates
@@ -38,6 +41,8 @@ app.use(
     store: store
   })
 );
+
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   console.log("checking...");
