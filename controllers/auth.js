2b31af1f064c6e8fc3f07a6216f83303a20053c3
Amanullah Aman <amancse.aiub@gmail.com>
2019-10-26 13:05:42 +0600

    express validatior setup and add validation with custom message at signup page

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,8 @@ const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 const sendGridTransport = require("nodemailer-sendgrid-transport");
 
+const { validationResult } = require("express-validator/check");
+
 const User = require("../models/user");
 
 //smtp gmail
@@ -85,6 +87,16 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg
+    });
+  }
 
   User.findOne({ email: email })
     .then(userDoc => {
